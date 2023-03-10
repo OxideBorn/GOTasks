3ae4da9065decd2935e5c9d55569e4d372a80dbc
OxideBorn <126889994+OxideBorn@users.noreply.github.com>
2023-03-10 14:39:13 +0300

    под вопросом
    Табл

---
+++ 222.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main(){
+	
+
+
+
+
+func intPow(base, exp int) int {
+    result := 1
+    for {
+        if exp & 1 == 1 {
+            result *= base
+        }
+        exp >>= 1
+        if exp == 0 {
+            break
+        }
+        base *= base
+    }
+
+    return result
+}
+}
